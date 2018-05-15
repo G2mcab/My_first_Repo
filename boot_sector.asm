0ba62170b6e310caefd36e09baf1549c5cf48cf8
G2mcab <34392821+G2mcab@users.noreply.github.com>
2018-05-15 09:48:34 +0300

    Add files via upload

---
+++ boot_sector.asm
@@ -0,0 +1,27 @@
+[org 0x7c00]
+
+mov bx,hello_world
+call print_string
+
+jmp $
+
+print_string:
+pusha
+mov ah,0x0e
+
+print_loop:
+mov al,[bx]
+cmp al,0
+je print_string_done
+int 0x10
+add bx,1
+jmp print_loop
+
+print_string_done:
+popa
+ret
+
+hello_world db "This is my first OS Mother fuckers", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
